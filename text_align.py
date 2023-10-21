f3f800f954e2d9e734e1d8b941b36f8038edf2a0
juancamiloguzmantoro <toro@lean-ocean.com>
2023-10-21 18:39:31 +0200

    add todays challenge

---
+++ text_align.py
@@ -0,0 +1,32 @@
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input('thickness: ')) #This must be an odd number
+if(thickness % 2 == 0):
+    raise Exception('thickness must be odd')
+if(thickness > 50):
+    raise Exception('thickness cant be more than 50')
+
+
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+
